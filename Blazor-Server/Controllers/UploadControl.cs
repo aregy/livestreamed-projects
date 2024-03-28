cae4699cc4ab3091e1c0b616429ab66b53f8957d
aregy <aregy@are.gy>
2024-03-28 13:15:29 -0700

    Livestreamed 2024-03-26
    As seen on https://www.youtube.com/watch?v=PsMMgC9Q8qI

---
+++ Blazor-Server/Controllers/UploadControl.cs
@@ -0,0 +1,82 @@
+﻿using devexBlazor.Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace devexBlazor.Controllers;
+public class ChunkMetadata
+{
+    public int Index { get; set; }
+    public int TotalCount { get; set; }
+    public int FileSize { get; set; }
+    public string? FileName { get; set; }
+    public string? FileType { get; set; }
+    public string? FileGuid { get; set; }
+}
+[Route("api/[controller]")]
+public class UploadController : Controller
+{
+    private readonly IWebHostEnvironment _HostingEnvironment;
+    FileUrlStorageService _FileUrlStorageService;
+    public UploadController(IWebHostEnvironment hostingEnvironment, FileUrlStorageService fileUrlStorageService)
+    {
+        _HostingEnvironment = hostingEnvironment;
+        _FileUrlStorageService = fileUrlStorageService;
+    }
+
+    [HttpPost]
+    [Route("UploadFile")]
+    [DisableRequestSizeLimit]
+    public ActionResult UploadFile(IFormFile imageUpload, string chunkMetadata)
+    {
+        var tempPath = Path.Combine(_HostingEnvironment.WebRootPath, "uploads");
+        // Removes temporary files
+        RemoveTempFilesAfterDelay(tempPath, new TimeSpan(0, 5, 0));
+
+        try
+        {
+            if (!string.IsNullOrEmpty(chunkMetadata))
+            {
+                var metadataObject = JsonConvert.DeserializeObject<ChunkMetadata>(chunkMetadata);
+                var tempFilePath = Path.Combine(tempPath, metadataObject.FileGuid + ".tmp");
+                if (!Directory.Exists(tempPath))
+                    Directory.CreateDirectory(tempPath);
+
+                AppendContentToFile(tempFilePath, imageUpload);
+
+                if (metadataObject.Index == (metadataObject.TotalCount - 1))
+                {
+                    ProcessUploadedFile(tempFilePath, metadataObject.FileName);
+                    _FileUrlStorageService.Add(Guid.Parse(metadataObject.FileGuid), @"uploads\" + metadataObject.FileName);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            return BadRequest();
+        }
+        return Ok();
+    }
+    void RemoveTempFilesAfterDelay(string path, TimeSpan delay)
+    {
+        var dir = new DirectoryInfo(path);
+        if (dir.Exists)
+            foreach (var file in dir.GetFiles("*.tmp").Where(f => f.LastWriteTimeUtc.Add(delay) < DateTime.UtcNow))
+                file.Delete();
+    }
+    void ProcessUploadedFile(string tempFilePath, string fileName)
+    {
+        var path = Path.Combine(_HostingEnvironment.WebRootPath, "uploads");
+        var imagePath = Path.Combine(path, fileName);
+        if (System.IO.File.Exists(imagePath))
+            System.IO.File.Delete(imagePath);
+        System.IO.File.Copy(tempFilePath, imagePath);
+    }
+    void AppendContentToFile(string path, IFormFile content)
+    {
+        using (var stream = new FileStream(path, FileMode.Append, FileAccess.Write))
+        {
+            content.CopyTo(stream);
+        }
+    }
+}
+
---
+++ Blazor-Server/Data/FileUrlStorageService.cs
@@ -0,0 +1,25 @@
+﻿namespace devexBlazor.Data;
+
+public class FileUrlStorageService
+{
+    private Dictionary<Guid, string> _FileUrlStorageStorage = new Dictionary<Guid, string>();
+    public void Add(Guid key, string val)
+    {
+        if (!_FileUrlStorageStorage.ContainsKey(key))
+            _FileUrlStorageStorage.Add(key, val);
+        ]
+    }
+    public string? Get(Guid key)
+    {
+        private int myVar;
+    
+    public int MyProperty
+    {
+        get { return myVar; }
+        set { myVar = value; }
+    }
+
+        return _FileUrlStorageStorage.GetValueOrDefault(key);
+    }
+}
+
