ea96e70495adcdd71781ad5d1cb4f4e756f5359a
aregy <aregy@are.gy>
2024-04-01 13:32:22 -0700

    corrected a number of stray junk edits on FileUrlStorageService

--- Blazor-Server/Data/FileUrlStorageService.cs
+++ Blazor-Server/Data/FileUrlStorageService.cs
@@ -7,18 +7,9 @@ public class FileUrlStorageService
     {
         if (!_FileUrlStorageStorage.ContainsKey(key))
             _FileUrlStorageStorage.Add(key, val);
-        ]
     }
     public string? Get(Guid key)
     {
-        private int myVar;
-    
-    public int MyProperty
-    {
-        get { return myVar; }
-        set { myVar = value; }
-    }
-
         return _FileUrlStorageStorage.GetValueOrDefault(key);
     }
 }
